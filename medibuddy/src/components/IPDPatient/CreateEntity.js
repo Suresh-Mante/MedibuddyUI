e9f215bcee244b8722bff1a23213dc42df8c2bc2
Soudipdas <aparnadas1211981@gmail.com>
2022-09-23 17:39:45 +0530

    IPDPatient/CreateEntity added

--- medibuddy/src/components/IPDPatient/CreateEntity.js
+++ medibuddy/src/components/IPDPatient/CreateEntity.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { getDataFromServer } from "../DataAccess";
-import { BASE_API, OPD_PATIENT_API } from "../Env";
+import { BASE_API, IPD_PATIENT_API } from "../Env";
 import Select from '../Shared/Select';
 import { formatDate, getOptionsArray } from "../Utils";
 import Loading from '../Shared/Loading';
@@ -8,17 +8,18 @@ import { Link, useLocation } from "react-router-dom";
 
 const CreateEntity = (props) => {
     //for editing
-    let existing_OPDPatient = null;
+    let existing_IPDPatient = null;
     const location = useLocation();
     if (props.editing) {
-        existing_OPDPatient = location.state;
+        existing_IPDPatient = location.state;
     }
     const [state, setState] = useState({
-        OPDPatient: {
-            pid: existing_OPDPatient ? existing_OPDPatient.pid : 0,
-            docId: existing_OPDPatient ? existing_OPDPatient.docId : 0,
-            visitDate: existing_OPDPatient ? formatDate(new Date(existing_OPDPatient.visitDate)) : formatDate(new Date()),
-            opdBillingID: existing_OPDPatient ? existing_OPDPatient.opdBillingID : 0,
+        IPDPatient: {
+            pid: existing_IPDPatient ? existing_IPDPatient.pid : 0,
+            docId: existing_IPDPatient ? existing_IPDPatient.docId : 0,
+            nurseID: existing_IPDPatient ? existing_IPDPatient.nurseID : 0,
+            entryDate: existing_IPDPatient ? formatDate(new Date(existing_IPDPatient.entryDate)) : formatDate(new Date()),
+            roomID: existing_IPDPatient ? existing_IPDPatient.roomID : 0,
         },
         patients: null,
         doctors: null,
@@ -28,37 +29,41 @@ const CreateEntity = (props) => {
         messages: [],
         updating: false
     });
-    const OPDPatientValid = () => {
+    const IPDPatientValid = () => {
         const validationErrors = [];
-        if (state.OPDPatient.pid <= 0) {
+        if (state.IPDPatient.pid <= 0) {
             validationErrors.push(`Patient id must be positive`);
         }
-        if (state.OPDPatient.docId <= 0) {
+        if (state.IPDPatient.docId <= 0) {
             validationErrors.push(`Doctor id must be positive`);
         }
-        if (state.OPDPatient.opdBillingID <= 0) {
-            validationErrors.push(`OPD BillingID must be positive`);
+        if (state.IPDPatient.nurseID <= 0) {
+            validationErrors.push(`Nurse id must be positive`);
+        }
+        if (state.IPDPatient.roomID <= 0) {
+            validationErrors.push(`RoomID must be positive`);
         }
 
         return validationErrors;
     }
-    const createOPDPatient = async () => {
-        const response = await getDataFromServer(OPD_PATIENT_API + `${existing_OPDPatient ? `/?id=${existing_OPDPatient.id}` : ''}`,
-            `${existing_OPDPatient ? 'PUT' : 'POST'}`, state.OPDPatient);
+    const createIPDPatient = async () => {
+        const response = await getDataFromServer(IPD_PATIENT_API + `${existing_IPDPatient ? `/?id=${existing_IPDPatient.id}` : ''}`,
+            `${existing_IPDPatient ? 'PUT' : 'POST'}`, state.IPDPatient);
         if (response) {
             if (response.statusCode == 201 || response.statusCode == 204) {
-                const OPDPatient = response.record;
+                const IPDPatient = response.record;
                 setState({
                     ...state,
-                    OPDPatient: existing_OPDPatient ? state.OPDPatient :
+                    IPDPatient: existing_IPDPatient ? state.IPDPatient :
                         {
                             pid: 0,
                             docId: 0,
-                            visitDate: formatDate(new Date()),
-                            opdBillingID: 0,
+                            nurseID: 0,
+                            entryDate: formatDate(new Date()),
+                            roomID: 0,
                         },
                     creating: false,
-                    messages: [`OPDPatient ${existing_OPDPatient ? 'updated' : 'created'} successfully...`]
+                    messages: [`IPDPatient ${existing_IPDPatient ? 'updated' : 'created'} successfully...`]
                 });
             } else {
                 setState({
@@ -73,8 +78,8 @@ const CreateEntity = (props) => {
             });
         }
     }
-    const createNewOPDPatient = () => {
-        const validationErrors = OPDPatientValid();
+    const createNewIPDPatient = () => {
+        const validationErrors = IPDPatientValid();
         if (validationErrors.length == 0) {
             setState({
                 ...state,
@@ -90,71 +95,83 @@ const CreateEntity = (props) => {
     }
     useEffect(() => {
         if (state.creating) {
-            createOPDPatient();
+            createIPDPatient();
         }
     }, [state.creating]);
     return (
         <>
             <div className="flex flex-align-center">
-                <Link to={'/OPDPatient'}>
+                <Link to={'/IPDPatient'}>
                     <svg viewBox="0 0 24 24" className='icon circle-bg' width='24px' height='24px'
                         style={{ marginRight: '5px' }}>
                         <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"
                             fill='currentColor'></path></svg>
                 </Link>
                 <div className='app-title flex flex-row flex-align-center flex-justify-center'>
-                    <span id='app-title-text'>{`${existing_OPDPatient ? 'Update' : 'Create'} OPDPatient`}</span>
+                    <span id='app-title-text'>{`${existing_IPDPatient ? 'Update' : 'Create'} IPDPatient`}</span>
                 </div>
             </div>
             <div className="container m-2 flex flex-column" style={{
                 gap: '5px'
             }}>
                 <div className="form-group">
-                    <label htmlFor="OPDPatientSharedCapacity">Patient Id</label>
-                    <input className="form-control" value={state.OPDPatient.pid} type={'number'}
+                    <label htmlFor="IPDPatientSharedCapacity">Patient Id</label>
+                    <input className="form-control" value={state.IPDPatient.pid} type={'number'}
                         onChange={(event) => setState({
                             ...state,
-                            OPDPatient: {
-                                ...state.OPDPatient,
+                            IPDPatient: {
+                                ...state.IPDPatient,
                                 pid: Number(event.target.value)
                             }
                         })}></input>
                 </div>
                 <div className="form-group">
-                    <label htmlFor="OPDPatientSharedCapacity">Doctor Id</label>
-                    <input className="form-control" value={state.OPDPatient.docId} type={'number'}
+                    <label htmlFor="IPDPatientSharedCapacity">Doctor Id</label>
+                    <input className="form-control" value={state.IPDPatient.docId} type={'number'}
                         onChange={(event) => setState({
                             ...state,
-                            OPDPatient: {
-                                ...state.OPDPatient,
+                            IPDPatient: {
+                                ...state.IPDPatient,
                                 docId: Number(event.target.value)
                             }
                         })}></input>
                 </div>
                 <div className="form-group">
-                    <label htmlFor="OPDPatientSharedCapacity">Visit Date</label>
-                    <input className="form-control" value={state.OPDPatient.visitDate} type={'date'} readOnly></input>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="OPDPatientSharedCapacity">OPDBilling Id</label>
-                    <input className="form-control" value={state.OPDPatient.opdBillingID} type={'number'}
+                    <label htmlFor="IPDPatientSharedCapacity">Nurse Id</label>
+                    <input className="form-control" value={state.IPDPatient.nurseID} type={'number'}
                         onChange={(event) => setState({
                             ...state,
-                            OPDPatient: {
-                                ...state.OPDPatient,
-                                opdBillingID: Number(event.target.value)
+                            IPDPatient: {
+                                ...state.IPDPatient,
+                                nurseID: Number(event.target.value)
                             }
                         })}></input>
                 </div>
-                <button className="btn btn-primary" onClick={createNewOPDPatient} disabled={state.creating}>
-                    {`${existing_OPDPatient ? 'Update' : 'Create'}`}
+                <div className="form-group">
+                    <label htmlFor="IPDPatientSharedCapacity">Entry Date</label>
+                    <input className="form-control" value={state.IPDPatient.entryDate} type={'date'} readOnly></input>
+                </div>
+                
+                <div className="form-group">
+                    <label htmlFor="IPDPatientSharedCapacity">Room Id</label>
+                    <input className="form-control" value={state.IPDPatient.roomID} type={'number'}
+                        onChange={(event) => setState({
+                            ...state,
+                            IPDPatient: {
+                                ...state.IPDPatient,
+                                roomID: Number(event.target.value)
+                            }
+                        })}></input>
+                </div>
+                <button className="btn btn-primary" onClick={createNewIPDPatient} disabled={state.creating}>
+                    {`${existing_IPDPatient ? 'Update' : 'Create'}`}
                 </button>
                 {
                     state.creating &&
                     <div className="container flex flex-align-center" style={{
                         gap: '10px'
                     }}>
-                        <em>{`${existing_OPDPatient ? 'Updating' : 'Creating'}`} OPDPatient...</em>
+                        <em>{`${existing_IPDPatient ? 'Updating' : 'Creating'}`} IPDPatient...</em>
                         <Loading />
                     </div>
                 }
